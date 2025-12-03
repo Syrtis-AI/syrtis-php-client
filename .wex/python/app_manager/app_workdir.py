a5fa5451ce732900482282c48435dde005aedd28
weeger <romain.weeger@gmail.com>
2025-12-03 19:29:41 +0100

    Config

--- .wex/python/app_manager/app_workdir.py
+++ .wex/python/app_manager/app_workdir.py
@@ -1,6 +1,20 @@
 from __future__ import annotations
 
-from wexample_wex_addon_dev_python.workdir.python_workdir import PythonWorkdir
+from wexample_config.const.types import DictConfig
+from wexample_wex_addon_dev_php.workdir.php_package_workdir import PhpPackageWorkdir
 
-class AppWorkdir(PythonWorkdir):
-    pass
+
+class AppWorkdir(PhpPackageWorkdir):
+    def prepare_value(self, raw_value: DictConfig | None = None) -> DictConfig:
+        from wexample_helpers.helpers.string import string_to_kebab_case
+
+        raw_value = super().prepare_value(raw_value=raw_value)
+
+        raw_value["git"] = {
+            "main_branch": "main",
+            "remote": [
+
+            ]
+        }
+
+        return raw_value
