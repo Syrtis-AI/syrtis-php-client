4f34a189c935f970acd148c59f555997e17ca5ad
weeger <romain.weeger@gmail.com>
2026-01-12 11:27:44 +0100

    Improve client

--- src/Repository/ProjectRepository.php
+++ src/Repository/ProjectRepository.php
@@ -6,7 +6,6 @@ namespace SyrtisClient\Repository;
 
 use SyrtisClient\Entity\Project;
 use Wexample\PhpApi\Common\AbstractApiRepository;
-use Wexample\PhpApi\Const\HttpMethod;
 
 class ProjectRepository extends AbstractApiRepository
 {
@@ -15,19 +14,4 @@ class ProjectRepository extends AbstractApiRepository
         return Project::class;
     }
 
-    /**
-     * @return Project[]
-     */
-    public function fetchList(): array
-    {
-        $data = $this->client->requestJson(
-            HttpMethod::GET,
-            $this->buildPath('list')
-        );
-
-        $payload = is_array($data['data'] ?? null) ? $data['data'] : $data;
-        $items = is_array($payload['items'] ?? null) ? $payload['items'] : [];
-
-        return $this->createFromApiCollection($items);
-    }
 }
